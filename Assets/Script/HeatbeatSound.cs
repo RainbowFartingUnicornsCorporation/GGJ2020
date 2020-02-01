8dbabcc5f71d09dde1caac4d4cca965d18b9ceba
B3ND3L <delahais.b@gmail.com>
2020-02-01 17:09:52 +0100

    add new sounds

---
+++ Assets/Script/HeatbeatSound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatbeatSound : MonoBehaviour
+
+{
+
+    public AudioSource beat1;
+    public AudioSource beat2;
+    
+    
+
+    public IEnumerator BoomBoom()
+    {
+        beat1.Stop();
+        beat2.Stop();
+
+        beat1.Play();
+        yield return new WaitWhile(() => beat1.isPlaying);
+        beat2.Play();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+}
