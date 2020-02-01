b226a0d84f2433ba2a852585d76334f3124989d0
thomas guibert <guibert.th@gmail.com>
2020-02-01 18:28:43 +0100

    [WIP] Add Brain test scene

---
+++ Assets/Script/PathFollower.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    public PathDrawer2 pathToFollow;
+    public bool paused = false;
+
+    public int CurrentWayPointId = 0;
+    public int MaxPointNumber = 0;
+    private float speed = 5.0f;
+    private float reachDistance = 1.0f;
+
+    private void Start()
+    {
+        MaxPointNumber = pathToFollow.path_objs.Count;
+    }
+
+    void Update()
+    {
+        if (paused) return;
+
+        var nextPosition = pathToFollow.path_objs[CurrentWayPointId].position;
+        float distance = Vector3.Distance(nextPosition, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, Time.deltaTime * speed);
+
+        if (distance <= reachDistance)
+            CurrentWayPointId++;
+
+        if (CurrentWayPointId >= pathToFollow.path_objs.Count)
+            CurrentWayPointId = 0;
+    }
+}
---
+++ Assets/Script/SynapseActivation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SynapseActivation : MonoBehaviour
+{
+    private const string ENERGY_DOT = "EnergyDot";
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == ENERGY_DOT)
+            GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == ENERGY_DOT)
+            GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
