728b360bbb0c452c06e57ac874cd7b3ba8f71155
B3ND3L <delahais.b@gmail.com>
2020-02-01 13:52:58 +0100

    Add scroll system for the left hear

---
+++ Assets/Script/ScrollBehavior.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBehavior : MonoBehaviour
+{
+
+    public AudioSource friction_1;
+    public AudioSource friction_2;
+    public float penality;
+    public float winCoefficient;
+
+    private float score;
+
+
+
+    void ComputeScore(float scrollScore)
+    {
+        print(score);
+        if(scrollScore == 0f)
+        {
+            score -= penality;
+        }
+        else
+        {
+            score += scrollScore;
+        }
+    }
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scrollScore = Input.GetAxis("Mouse ScrollWheel");
+        ComputeScore(Mathf.Abs(scrollScore) * winCoefficient);
+        if (!friction_1.isPlaying && !friction_2.isPlaying)
+        {
+            if (scrollScore > 0f)
+            {
+                friction_1.Play();
+            } else if (scrollScore < 0f)
+            {
+                friction_2.Play();
+            }
+        }
+    }
+}
